a7daa73074d877e1a486f91c0fb39fd3f908ab12
Adikko <adriankulik93@gmail.com>
2021-04-02 20:51:40 +0200

    animations are picked depending on elapsed seconds

--- index.js
+++ index.js
@@ -35,52 +35,51 @@ directionalLight.position.set(1, 1, 5).normalize();
 scene.add(directionalLight);
 
 // model
-let mesh = null; //global variables
-let mesh2 = null;
+let logo_opened = null; //global variables
+let logo_opened_copy1 = null;
 let logo_closed_anim = null;
 
 const mtlLoader = new THREE.MTLLoader();
-mtlLoader.load('./3D/opened_no_modifier.mtl', function(materials) {
+mtlLoader.load('./3D/3d_logo_opened.mtl', function(materials) {
 
   materials.preload();
 
   const objLoader = new THREE.OBJLoader();
   objLoader.setMaterials(materials);
-  objLoader.load('./3D/opened_no_modifier.obj', function(object) {
+  objLoader.load('./3D/3d_logo_opened.obj', function(object) {
 
-    mesh = object; //accessing the global variable
-    mesh.position.x = 100;
-    mesh.position.y = 100;
-    mesh.position.z = -100;
-    mesh.scale.x = 10;
-    mesh.scale.y = 10;
-    mesh.scale.z = 10;
-    mesh.rotation.x = Math.PI * 0.25;
-    mesh.rotation.y = Math.PI * 0.25;
-    scene.add(mesh);
+    logo_opened = object; //accessing the global variable
+    logo_opened.position.x = 110;
+    logo_opened.position.y = -50;
+    logo_opened.position.z = -100;
+    logo_opened.scale.x = 10;
+    logo_opened.scale.y = 10;
+    logo_opened.scale.z = 10;
+    logo_opened.rotation.x = Math.PI * 0.25;
+    logo_opened.rotation.y = Math.PI * 0.25;
+    //scene.add(logo_opened);
 
   });
 
 });
 
-mtlLoader.load('./3D/opened_no_modifier_orange.mtl', function(materials) {
+mtlLoader.load('./3D/3d_logo_opened_teal.mtl', function(materials) {
 
   materials.preload();
 
   const objLoader = new THREE.OBJLoader();
   objLoader.setMaterials(materials);
-  objLoader.load('./3D/opened_no_modifier_orange.obj', function(object) {
+  objLoader.load('./3D/3d_logo_opened_detailed.obj', function(object) {
 
-    logo = object; //accessing the global variable
-    mesh2.position.y = 60;
-    mesh2.position.x = 50;
-    mesh2.position.z = -90;
-    mesh2.scale.x = 5;
-    mesh2.scale.y = 5;
-    mesh2.scale.z = 5;
-    mesh2.rotation.x = Math.PI * 0.25;
-    mesh2.rotation.y = Math.PI * -0.25;
-    scene.add(mesh2);
+    logo_opened_copy1 = object;
+    logo_opened_copy1.position.x = 0;
+    logo_opened_copy1.position.y = 30;
+    logo_opened_copy1.position.z = -1000;
+    logo_opened_copy1.scale.x = 50;
+    logo_opened_copy1.scale.y = 200;
+    logo_opened_copy1.scale.z = 50;
+    logo_opened_copy1.rotation.x = Math.PI * 0.25;
+    logo_opened_copy1.rotation.y = Math.PI * 0.75;
 
   });
 
@@ -95,7 +94,7 @@ mtlLoader.load('./3D/3d_logo_closed.mtl', function(materials) {
   objLoader.load('./3D/3d_logo_closed.obj', function(object) {
 
     logo_closed_anim = object; //accessing the global variable
-    logo_closed_anim.position.y = 60;
+    logo_closed_anim.position.y = 10;
     logo_closed_anim.position.x = 0;
     logo_closed_anim.position.z = 0;
     logo_closed_anim.scale.x = 30;
@@ -103,7 +102,7 @@ mtlLoader.load('./3D/3d_logo_closed.mtl', function(materials) {
     logo_closed_anim.scale.z = 30;
     logo_closed_anim.rotation.x = Math.PI * 0.25;
     logo_closed_anim.rotation.y = Math.PI * 0.75;
-    scene.add(logo_closed_anim);
+    //scene.add(logo_closed_anim);
 
   });
 
@@ -122,6 +121,10 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setClearColor(0x000000);
 renderer.setSize(sizes.width, sizes.height);
 
+//date
+let Landing_date = new Date();
+let seconds = Landing_date.getSeconds();
+
 //clock
 const clock = new THREE.Clock(); //importing the clock in order to make the animations take as much time on any refresh rate screen 
 
@@ -131,17 +134,24 @@ const tick = () => {
   //measuring time
   const elapsedTime = clock.getElapsedTime();
 
-  if (mesh !== null) {
-    mesh.scale.y = mesh.scale.y + 0.05;
-  }
-
-  if (mesh2 !== null) {
-    mesh2.scale.y = mesh2.scale.y + 0.2;
-  }
-
-  if (logo_closed_anim !== null) {
-    logo_closed_anim.scale.y = (Math.cos(elapsedTime) + Math.PI) * 40;
-    logo_closed_anim.rotation.y = logo_closed_anim.rotation.y + 0.005;
+  if (seconds % 5 === 0) { //this is a fun one; depending on the division remainder we choose different animations!
+    if (logo_opened !== null) {
+      if (elapsedTime < 10) { //limiting the final y scale of the mesh (by using a set amount of secounds), so it doesn't render indefinately
+        scene.add(logo_opened);
+        logo_opened.scale.y = logo_opened.scale.y + (elapsedTime * 0.05);
+      }
+    }
+  } else if (seconds % 3 === 0) {
+    if (logo_closed_anim !== null) {
+      scene.add(logo_closed_anim);
+      logo_closed_anim.scale.y = (Math.cos(elapsedTime) + Math.PI) * 20;
+      logo_closed_anim.rotation.y = logo_closed_anim.rotation.y + 0.005;
+    }
+  } else {
+    if (logo_opened_copy1 !== null) {
+      scene.add(logo_opened_copy1);
+      logo_opened_copy1.rotation.y = logo_opened_copy1.rotation.y + 0.001;
+    }
   }
 
   //render
